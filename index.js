59ca44fed45ed58e654cc6c0061254d262bc383d
Alexander Skrabl <alexander@skrabl.de>
2016-04-23 23:03:02 +0100

    just testing out some rxjs

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 
-console.log('test');
+import Rx from 'rx';
+
+const button = document.getElementById('clickMeButton');
+const clickStream = Rx.Observable.fromEvent(button, 'click');
+
+clickStream.subscribe( e => {
+    console.log(e, 'click');
+});
