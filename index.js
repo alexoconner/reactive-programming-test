ec1cd969ca6c62b49805f13eb6ab21a0a3f357f3
Alexander Skrabl <alexander@skrabl.de>
2016-05-08 22:43:02 +0100

    trying out double click with rx

--- index.js
+++ index.js
@@ -5,5 +5,14 @@ const button = document.getElementById('clickMeButton');
 const clickStream = Rx.Observable.fromEvent(button, 'click');
 
 clickStream.subscribe( e => {
-    console.log(e, 'click');
+    console.log(e, ' click');
+});
+
+let doubleClick = clickStream
+    .buffer( () => clickStream.debounce(300) )
+    .map( clickEvents => clickEvents.length )
+    .filter( x => x === 2 );
+
+doubleClick.subscribe( e => {
+    console.log(e, 'double click');
 });
